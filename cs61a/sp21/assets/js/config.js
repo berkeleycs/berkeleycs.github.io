6a51ae20659c82d80a1d1fcf1791d2299f6e452c
Joe Mo <49494600+yuzhoumo@users.noreply.github.com>
2024-01-28 22:37:04 +0000

    add ./cs61a/sp21

---
+++ cs61a/sp21/assets/js/config.js
@@ -0,0 +1,8 @@
+/* Start/end date of semester. Make sure to change utils.py as well */
+var startDate = '2021-01-17', endDate = '2021-05-13';
+
+/* Calendar Specification */
+var officeHoursCalendarId = 'c_q0g2eb9m02lofv8741cmi9o374@group.calendar.google.com';
+var instructorOfficeHoursCalendarId = 'c_mna7tgn21s1epp7njhth4rd6vc@group.calendar.google.com';
+var lecturesCalendarId = 'c_9rk5gljt907809mvjg4k0jjs10@group.calendar.google.com';
+var sectionsCalendarId = 'c_1emvvbjtg656lq9nae6lqt3er4@group.calendar.google.com';
